7f0477c9e13f1acdaecb26783603b116972d8c90
Christian Hinkle <christianhinko@gmail.com>
2025-12-05 10:57:10 -0700

    [cpp][todo] Document options for resolving todo about `constexpr` support.

--- Include/Include/CppUtils_Core.h
+++ Include/Include/CppUtils_Core.h
@@ -19,7 +19,10 @@ namespace CppUtils::Core
     /**
      * @brief A software implementation of the IEEE 754 standard's divide operation to support special
      *        cases like NaN and inf values.
-     * @todo @Christian TODO: [todo][cpp] Convert this to a constexpr function, as it is basic arithmetic.
+     * @todo @Christian TODO: [todo][cpp] Convert this to a constexpr function, as it is basic arithmetic. There are
+     *       two options for this: either (1) make our own `constexpr` versions of the `std` functions that aren't
+     *       constexpr yet, or (2) wait for C++23 which will make many of the `std` functions `constexpr`. I'm just going
+     *       to wait for C++23 support, unless we really need to have this for some reason.
      */
     template <CppUtils::Concepts::Std::floating_point TFloat>
     TFloat IeeeDivide(TFloat dividend, TFloat divisor);
