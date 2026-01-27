0d8d3d5bcc57a2ddb63413db1cc8d2e4dac7c513
Christian Hinkle <christianhinko@gmail.com>
2026-01-27 04:27:40 -0700

    [cpp][cmake] Write simple `OnScopeExit` RAII-based struct and function for users to schedule logic to run at the end of their scope.

---
+++ Include/Include/CppUtils_Core/Raii.h
@@ -0,0 +1,53 @@
+// Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
+
+#pragma once
+
+#include <CppUtils_StdReimpl/Concepts.h>
+#include <utility>
+#include <type_traits>
+
+namespace CppUtils::Core
+{
+    /**
+     * @brief @Christian: TODO: [todo][tests] No tests are written for this utility, and it's not currently used anywhere.
+     */
+    template <CppUtils::StdReimpl::Concepts::invocable TFunctor>
+    struct OnScopeExitStruct final
+    {
+        OnScopeExitStruct(const OnScopeExitStruct&) = delete;
+        OnScopeExitStruct(OnScopeExitStruct&&) noexcept = delete;
+
+        OnScopeExitStruct& operator=(const OnScopeExitStruct&) = delete;
+        OnScopeExitStruct& operator=(OnScopeExitStruct&&) noexcept = delete;
+
+        // Construct from rvalue callback.
+        OnScopeExitStruct(TFunctor&& callback)
+            : Callback(std::move(callback))
+        {
+        }
+        // Construct from lvalue callback.
+        OnScopeExitStruct(const TFunctor& callback)
+            : Callback(callback)
+        {
+        }
+
+        // Destructor, which invokes the callback.
+        ~OnScopeExitStruct()
+        {
+            std::move(Callback)();
+        }
+
+    private:
+
+        TFunctor Callback;
+    };
+
+    /**
+     * @brief @Christian: TODO: [todo][tests] No tests are written for this utility, and it's not currently used anywhere.
+     */
+    template <CppUtils::StdReimpl::Concepts::invocable TFunctor>
+    decltype(auto) OnScopeExit(TFunctor&& callback)
+    {
+        return OnScopeExitStruct<std::decay_t<TFunctor>>(std::forward<TFunctor>(callback));
+    }
+}
