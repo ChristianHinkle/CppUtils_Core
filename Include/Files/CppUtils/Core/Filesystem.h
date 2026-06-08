a94daf4560d3b1bba73e6b7227e7e5719242bca5
Christian Hinkle <christianhinko@gmail.com>
2026-06-07 18:03:31 -0600

    [filesystem][cpp][migration] Bring in filesystem code from MeddySDK related to pretty path formating. And improve some of it (to provide the `PrettyPathSeparator` as a templated variable, and to use std::filesystem for path separator code instead of boost::filesystem, as that is unnecessary for such a simple task).

--- Include/Files/CppUtils/Core/Filesystem.h
+++ Include/Files/CppUtils/Core/Filesystem.h
@@ -9,6 +9,11 @@
 #include <CppUtils/Core/Concepts.h>
 
 /**
+ * @brief String literal alternative to `CppUtils::PrettyPathSeparator`.
+ */
+#define CPPUTILS_PRETTY_PATH_SEPARATOR '/'
+
+/**
  * @brief Filesystem utilities to extend the default std::filesystem tools.
  */
 namespace CppUtils
@@ -16,11 +21,20 @@ namespace CppUtils
     using FilesystemCharType = std::filesystem::path::value_type;
     using StdPathStringView = std::basic_string_view<FilesystemCharType>;
 
+    /**
+     * @brief A character that represents the most widely-accepted path separator across all platforms.
+     */
+    template <CppUtils::CharLike TChar = FilesystemCharType>
+    constexpr TChar PrettyPathSeparator = CPPUTILS_PRETTY_PATH_SEPARATOR;
+
     CPPUTILS_CORE_EXPORT StdPathStringView GetStringViewFromPath(const std::filesystem::path& path);
+
+    template <class TFwdIt, CppUtils::CharLike TChar = char>
+    void ConvertPathStringToPrettyFormat(TFwdIt begin, TFwdIt end);
 
     /**
      * @brief Create an empty file.
-     * @remark Easy overload for the typical use case to allows implicit conversions to the string view.
+     * @remark Easy overload for the typical use case to allow implicit conversions to the string view.
      */
     CPPUTILS_CORE_EXPORT bool TouchNewFile(const StdPathStringView path);
 
--- Include/Files/CppUtils/Core/Filesystem.inl
+++ Include/Files/CppUtils/Core/Filesystem.inl
@@ -4,7 +4,18 @@
 
 #include <CppUtils/Core/Filesystem.h>
 
+#include <algorithm>
 #include <fstream>
+
+template <class TFwdIt, CppUtils::CharLike TChar>
+void CppUtils::ConvertPathStringToPrettyFormat(TFwdIt begin, TFwdIt end)
+{
+    std::replace(
+        begin,
+        end,
+        static_cast<TChar>(std::filesystem::path::preferred_separator),
+        static_cast<TChar>(PrettyPathSeparator<TChar>));
+}
 
 template <CppUtils::CharLike TChar, class TCharTraits>
 bool CppUtils::TouchNewFile(const std::basic_string_view<TChar, TCharTraits> path)
